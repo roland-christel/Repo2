dad007a6a84bfaa70bb66cb0fa68f65837abf4c6
Roland Christel Sonounameto <roland.sonounameto@outlook.com>
2024-01-11 11:32:26 +0100

    adding packages

---
+++ script1.R
@@ -0,0 +1,18 @@
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++###
+##CODE R                                                                     ###
+##                                                                           ###
+#                                                                            ###
+##                                                                           ###
+## Updated: September, 2023                                                  ###
+##+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++###
+
+rm(list=ls())
+
+# Packages
+if(!require("readxl")){install.packages("readxl")}
+if(!require("tidyverse")){install.packages("tidyverse")}
+if(!require("ggtext")){install.packages("ggtext")}
+if(!require("patchwork")){install.packages("patchwork")}
+if(!require("mfx")){install.packages("mfx")}
+if(!require("aod")){install.packages("aod")}
+if(!require("ROSE")){install.packages("ROSE")}
